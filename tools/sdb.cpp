93e2b278813e12b7eb0507ab995ef88c0a7579d2
Patrick Kariuki <pkariuk1@jhu.edu>
2026-03-20 19:46:57 -0400

    exposing register reading and help

--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -4,6 +4,8 @@
 
 #include <algorithm>
 #include <editline/readline.h>
+#include <fmt/format.h>
+#include <fmt/ranges.h>
 #include <iostream>
 #include <sstream>
 #include <cstring>
@@ -63,6 +65,79 @@ namespace {
         std::cout << std::endl;
     }
 
+    void print_help(const std::vector<std::string>& args) {
+        if (args.size() == 1) {
+            std::cerr << R"(Available commands:
+    continue    - Resume the process
+    register    - Commands for operating on registers
+)";
+        }
+
+        else if (is_prefix(args[1], "register")) {
+            std::cerr << R"(Available commands:
+    read
+    read <register>
+    read all
+    write <register> <value>
+)";
+        }
+        else {
+            std::cerr << "No help available on that" << std::endl;
+        }
+    }
+
+    void handle_register_read(sdb::process& process,
+        const std::vector<std::string>& args) {
+        auto format = [](auto t) {
+            if constexpr (std::is_floating_point_v<decltype(t)>) {
+                return fmt::format("{}", t);
+            } else if constexpr (std::is_integral_v<decltype(t)>) {
+                return fmt::format("{:#0{}x}", t, sizeof(t) * 2 + 2);
+            } else {
+                return fmt::format("[{:#04x}]", fmt::join(t, ","));
+            }
+        };
+
+        if (args.size() == 2 or
+            (args.size() == 3 && args[2] == "all")) {
+            for (auto& info : sdb::g_register_infos) {
+                auto should_print = (args.size() == 3 or
+                    info.type == sdb::register_type::gpr) and
+                    info.name != "orig_rax";
+                if (!should_print) continue;
+                auto value = process.get_registers().read(info);
+                fmt::print("{}:\t{}\n", info.name, std::visit(format, value));
+            }
+        } else if (args.size() == 3) {
+            try {
+                auto info = sdb::register_info_by_name(args[2]);
+                auto value = process.get_registers().read(info);
+                fmt::print("{}:\t{}\n", info.name, std::visit(format, value));
+            } catch (sdb::error& err) {
+                std::cerr << "No such register" << std::endl;
+                return;
+            }
+        } else {
+            print_help({ "help", "register" });
+        }
+    }
+
+    void handle_register_command(sdb::process& process,
+        const std::vector<std::string>& args) {
+        if (args.size() < 2) {
+            print_help({ "help", "register" });
+            return;
+        }
+
+        if (is_prefix(args[1], "read")) {
+            handle_register_read(process, args);
+        } else if (is_prefix(args[1], "write")) {
+            handle_register_write(process, args);
+        } else {
+            print_help({ "help", "register" });
+        }
+    }
+
     void handle_command(std::unique_ptr<sdb::process>& process,
         std::string_view line) {
         auto args = split(line, ' ');
@@ -72,6 +147,10 @@ namespace {
             process->resume();
             auto reason = process->wait_on_signal();
             print_stop_reason(*process, reason);
+        } else if (is_prefix(command, "help")) {
+            print_help(args);
+        } else if (is_prefix(command, "register")) {
+            handle_register_command(*process, args);
         } else {
             std::cerr << "Unknown command: " << command << std::endl;
         }
