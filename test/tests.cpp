8c0dbf50227b133dfdf76b17971d78d1e647abd3
Patrick Kariuki <pkariuk1@jhu.edu>
2026-07-30 12:19:05 -0400

    testing watchpoints

--- test/tests.cpp
+++ test/tests.cpp
@@ -435,3 +435,37 @@ TEST_CASE("Hardware breakpoints evade memory checksums", "[breakpoint]") {
     REQUIRE(to_string_view(channel.read()) ==
         "Putting pineapple on pizza...\n");
 }
+
+TEST_CASE("Watchpoint detects read", "[watchpoint]") {
+    bool close_on_exec = false;
+    sdb::pipe channel(close_on_exec);
+    auto proc = process::launch(
+        "targets/anti_debugger", true, channel.get_write());
+    channel.close_write();
+
+    proc->resume();
+    proc->wait_on_signal();
+
+    auto func = virt_addr(
+        from_bytes<std::uint64_t>(channel.read().data()));
+    auto& watch = proc->create_watchpoint(
+        func, sdb::stoppoint_mode::read_write, 1);
+    watch.enable();
+
+    proc->resume();
+    proc->wait_on_signal(); // hit watchpoint
+
+    proc->step_instruction();
+    auto& soft = proc->create_breakpoint_site(func, false);
+    soft.enable();
+
+    proc->resume();
+    auto reason = proc->wait_on_signal(); // hit software breakpoint
+
+    REQUIRE(reason.info == SIGTRAP);
+
+    proc->resume();
+    proc->wait_on_signal();
+
+    REQUIRE(to_string_view(channel.read()) == "Putting pineapple on pizza...\n");
+}
